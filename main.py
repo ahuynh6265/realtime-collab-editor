1d790b44f4342c7f357cf22206aa79c4db24d382
Alex Huynh <ahuynh6265@users.noreply.github.com>
2026-03-12 14:36:13 -0400

    added user connect/disconnect notif, user count in each room

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from database import engine, SessionLocal, Base
 from models import Document
-
+import json
 
 connected_clients = {}
 
@@ -42,20 +42,40 @@ async def websocket_endpoint(websocket: WebSocket, document_id: int, username: s
     room = Document(text = "")
     db.add(room)
     db.commit() 
-  await websocket.send_text(room.text)
+  data = {"type": "update", "content": room.text}
+  await websocket.send_text(json.dumps(data))
 
   connected_clients[document_id][websocket] = username
+  data = {"type": "notification", "message": f"{username} has joined"}
+  for client in connected_clients[document_id].keys():
+    if client != websocket:
+      await client.send_text(json.dumps(data))
+
+  data = {"type": "users", "users": list(connected_clients[document_id].values())}
+  for client in connected_clients[document_id].keys():
+    await client.send_text(json.dumps(data))
+
   print(f"{username} connected to room {document_id} total: {len(connected_clients[document_id])}")
 
   try:
     while True:
       message = await websocket.receive_text()
+      data = {"type": "update", "content": message}
       for client in connected_clients[document_id].keys():
         if client != websocket: 
-          await client.send_text(message)
+          await client.send_text(json.dumps(data))
       room.text = message
       db.commit() 
   except WebSocketDisconnect: 
     pass
   finally:
+    data = {"type": "notification", "message": f"{username} has disconnected"}
+    for client in connected_clients[document_id].keys():
+      if client != websocket:
+        await client.send_text(json.dumps(data))
+    
     del connected_clients[document_id][websocket]
+
+    data = {"type": "users", "users": list(connected_clients[document_id].values())}
+    for client in connected_clients[document_id].keys():
+      await client.send_text(json.dumps(data))
